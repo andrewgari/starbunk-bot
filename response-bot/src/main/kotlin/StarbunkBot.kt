46719b935e93260dec84cd11cbf7bea542dcd9d9
Andrew Gari <35663269+CovaDax1@users.noreply.github.com>
2021-10-12 10:38:42 -0400

    Added Documentation

--- response-bot/src/main/kotlin/StarbunkBot.kt
+++ response-bot/src/main/kotlin/StarbunkBot.kt
@@ -6,14 +6,14 @@ import kotlinx.coroutines.reactor.mono
 
 
 fun main(args: Array<String>) {
-    val token = args[0]
-    val client = DiscordClient.create(token)
+    val token = args[0] // pulls token found on the Discord Bot Portal from the command line on startup
+    val client = DiscordClient.create(token) // creates a DiscordClient using the provided Token
 
-    client.withGateway {
-        mono {
-            it.on(MessageCreateEvent::class.java)
-                .asFlow()
-                .collect {
+    client.withGateway { // creates a connection to Discord's API
+        mono { // create a publisher object that emits the following
+            it.on(MessageCreateEvent::class.java) // emits a MessageCreateEvent Object when the bot detects one
+                .asFlow() // converts published events to a stream
+                .collect {  // handles the events found in the stream
                     val message = it.message
                 }
         }
